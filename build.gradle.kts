0349b98ac11a4eb5874527844380b69c9a1da499
Vadim Briliantov <vadim.briliantov@jetbrains.com>
2025-10-16 03:32:17 +0200

    Update version to future 0.5.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ group = "ai.koog"
 version = run {
     // our version follows the semver specification
 
-    val main = "0.5.0"
+    val main = "0.5.2"
 
     val feat = run {
         val releaseBuild = !System.getenv("BRANCH_KOOG_IS_RELEASING_FROM").isNullOrBlank()
